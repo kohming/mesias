76465787d33db38372aab3ad969bb25f7e19d642
Giovanizefa3 <vlrn250998@gmail.com>
2019-09-27 15:59:02 +0700

    first Commit

--- app/src/main/java/com/gka/gkamobile/MainActivity.java
+++ app/src/main/java/com/gka/gkamobile/MainActivity.java
@@ -1,6 +1,9 @@
 package com.gka.gkamobile;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -20,11 +23,18 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        Button button =(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(), "Tunngu Sebentar", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
-}
